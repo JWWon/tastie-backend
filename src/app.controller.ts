de7e6575b527711ab0bced73c34cd172f85b49ed
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-01-20 12:00:13 +0000

    Remove unnecessary methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from '@/app.service';
 
 @Controller()
-// eslint-disable-next-line
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'hello';
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  lintTest(): string {
-    return 'abcd';
-  }
-}
+export class AppService {}
