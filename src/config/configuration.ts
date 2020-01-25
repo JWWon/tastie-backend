e6776e6c19f5f977de9480574691d57785545225
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-01-25 00:44:05 +0000

    Add config about google place api

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,11 @@
 const defaultPort = 3000;
+const defaultGoogleOutputFormat = 'json';
 
 export default () => ({
   port: parseInt(process.env.PORT, 10) || defaultPort,
+  google: {
+    apiKey: process.env.GOOGLE_PLACES_API_KEY,
+    format:
+      process.env.GOOGLE_PLACES_OUTPUT_FORMAT || defaultGoogleOutputFormat,
+  },
 });
