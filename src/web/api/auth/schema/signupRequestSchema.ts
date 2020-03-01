8988ebb8d16bafe9dadef5105af775fa9a3c5537
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-01 01:55:03 +0000

    Add password regular expression

--- src/web/api/auth/schema/signupRequestSchema.ts
+++ src/web/api/auth/schema/signupRequestSchema.ts
@@ -2,6 +2,13 @@ import * as Joi from 'joi';
 import { AccessTokenRequestSchema } from './accessTokenRequestSchema';
 
 export const SignupRequestSchema = AccessTokenRequestSchema.keys({
+  password: Joi.when('type', {
+    is: 'email',
+    then: Joi.string()
+      .regex(new RegExp('^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$'))
+      .required(),
+  }),
+
   name: Joi.string()
     .required()
     .min(2)
