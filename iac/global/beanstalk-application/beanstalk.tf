ee41b45aed3729b9b1e9045aa1bcc1fc812ec7ea
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-08 13:10:35 +0000

    Add iam instance profile

--- iac/global/beanstalk-application/beanstalk.tf
+++ iac/global/beanstalk-application/beanstalk.tf
@@ -7,3 +7,13 @@ resource "aws_elastic_beanstalk_application" "this" {
   name        = local.application_name
   description = local.application_description
 }
+
+resource "aws_iam_role" "ec2" {
+  name               = "aws-iam-role-eb"
+  assume_role_policy = data.aws_iam_policy_document.ec2.json
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "iam-instance-profile-eb"
+  role = aws_iam_role.ec2.name
+}
