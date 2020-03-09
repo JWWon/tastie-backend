a02807cf68ef4fc53506976a818deef6fef5ba55
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-09 14:22:40 +0000

    Apply terraform fmt

--- iac/staging/beanstalk-environment/main.tf
+++ iac/staging/beanstalk-environment/main.tf
@@ -13,12 +13,12 @@ module "beanstalk_environment" {
   source  = "app.terraform.io/tastie/eb-backend/aws"
   version = "0.0.8"
 
-  name          = "staging"
+  name                = "staging"
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.14.2 running Docker 18.09.9-ce"
 
-  instance_type = "t2.micro"
-  application_port = "80"
-  healthcheck_url = "/api/app/healthy"
+  instance_type                = "t2.micro"
+  application_port             = "80"
+  healthcheck_url              = "/api/app/healthy"
   loadbalancer_certificate_arn = var.loadbalancer_certificate_arn
 
   google_place_apikey = var.google_place_apikey
