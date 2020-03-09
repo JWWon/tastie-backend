7e04154515b03f96d539c3fd6f2d8fe41ba59fde
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-09 11:42:59 +0000

    Separate between beanstalk-application and ec2-profile

--- iac/global/ec2-profile/data.tf
+++ iac/global/ec2-profile/data.tf
@@ -1,31 +1,25 @@
-data "aws_iam_policy_document" "ec2" {
+data "aws_iam_policy_document" "this" {
   statement {
     sid = ""
-
     actions = [
       "sts:AssumeRole",
     ]
-
     principals {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
-
     effect = "Allow"
   }
 
   statement {
     sid = ""
-
     actions = [
       "sts:AssumeRole",
     ]
-
     principals {
       type        = "Service"
       identifiers = ["ssm.amazonaws.com"]
     }
-
     effect = "Allow"
   }
 }
