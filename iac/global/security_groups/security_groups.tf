ba52cb32774aa55c439d17320470004b87c147a2
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-14 06:27:26 +0000

    Fix ingress options

--- iac/global/security_groups/security_groups.tf
+++ iac/global/security_groups/security_groups.tf
@@ -3,10 +3,10 @@ resource "aws_security_group" "allow_postgres" {
   description = "Allow postgres inbound traffic"
 
   ingress {
-    description = "Postgres port"
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
