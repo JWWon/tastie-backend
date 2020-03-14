f8ed0888da556f3caa6804707d948555f936ec68
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-14 06:44:40 +0000

    Update beanstalk-environment variables

--- iac/prod/beanstalk-environment/variables.tf
+++ iac/prod/beanstalk-environment/variables.tf
@@ -1,6 +1,6 @@
 variable "loadbalancer_certificate_arn" { type = string }
 variable "google_place_apikey" { type = string }
-# variable "postgres_dbname" { type = string }
-# variable "postgres_host" { type = string }
-# variable "postgres_password" { type = string }
-# variable "postgres_username" { type = string }
+variable "postgres_dbname" { type = string }
+variable "postgres_host" { type = string }
+variable "postgres_password" { type = string }
+variable "postgres_username" { type = string }
