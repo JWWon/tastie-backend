0a2d7c9f170c3d33b2d1edcf9032f8d4a3c7c35f
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-04-02 14:03:03 +0000

    Support mongodb typeorm for place storing

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import {
   CaseModule,
   AuthModule,
   UserModule,
+  RestaurantModule,
 } from './module';
 
 @Module({
@@ -19,6 +20,7 @@ import {
     CaseModule,
     AuthModule,
     UserModule,
+    RestaurantModule,
     ConfigModule.forRoot({
       load: [config],
       isGlobal: true,
@@ -35,6 +37,15 @@ import {
         database: configService.get('db.database'),
         entities: [`${__dirname}/infrastructure/typeorm/model/*{.ts,.js}`],
         synchronize: configService.get('db.synchronize'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb' as 'mongodb',
+        url: configService.get('mongo.url'),
+        ssl: true,
       }),
     }),
   ],
