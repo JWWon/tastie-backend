6f68c716f0f0c20911c5d6702a40e14c889caf18
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-04-04 01:24:01 +0000

    Synchronize environment variable in beanstalk configuration

--- iac/staging/beanstalk-environment/main.tf
+++ iac/staging/beanstalk-environment/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 module "beanstalk_environment" {
   source  = "app.terraform.io/tastie/eb-backend/aws"
-  version = "0.0.8"
+  version = "0.1.0"
 
   name                = "staging"
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.14.2 running Docker 18.09.9-ce"
@@ -26,4 +26,7 @@ module "beanstalk_environment" {
   postgres_dbname     = var.postgres_dbname
   postgres_username   = var.postgres_username
   postgres_password   = var.postgres_password
+  sendinblue_apikey   = var.sendinblue_apikey
+  listen_port         = var.listen_port
+  mongo_url           = var.mongo_url
 }
